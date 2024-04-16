0c765a23a2dbe679156560c63a364431ef2a3019
Andreas Kloeckner <inform@tiker.net>
2024-04-16 09:19:55 -0500

    Add a benchmark script

---
+++ examples/benchmark.py
@@ -0,0 +1,28 @@
+from time import process_time_ns
+
+from indexset import IndexSet
+n = 10000000
+
+def test_create(tp):
+    start = process_time_ns()
+    for i in range(n):
+        tp()
+    stop = process_time_ns()
+
+    print(f"create {tp}: {(stop-start)/n} ns/round")
+
+def test_add(tp):
+    s = tp()
+    start = process_time_ns()
+    for i in range(n):
+        s.add(i)
+    stop = process_time_ns()
+
+    print(f"add: {tp}: {(stop-start)/n} ns/round")
+
+if 1:
+    for tp in [set, IndexSet]:
+        test_create(tp)
+        test_add(tp)
+else:
+    test_add(IndexSet)
